4915684f8fa1fe3c62809b22e85513c49b025c77
baHa <baho908@gmail.com>
2019-04-23 00:17:57 +0300

    güncelle ve verial fonksiyonu güncellendi

--- 2. semester/project1.c
+++ 2. semester/project1.c
@@ -35,7 +35,8 @@ int main(){
 	// for (i=0; i<100; i++){
 	// 	HesapNoOlustur();
 	// }
-	//Guncelle();
+	VeriAl();
+	Guncelle();
 }
 void HesapNoOlustur(){
 	int temp, islem=0;
@@ -86,7 +87,7 @@ void VeriAl(){
 }
 
 void Guncelle(){
-	int i;
+	int i, j, b=0, t=0;
 	FILE *pf1, *pf2;
 	fclose(fopen("bireyselMusteri.txt", "w"));
 	fclose(fopen("ticariMusteri.txt", "w"));
@@ -94,9 +95,25 @@ void Guncelle(){
 	pf2 = fopen("ticariMusteri.txt", "a");
 	for (i=0; i<aBank.mSayisi; i++){
 		if (aBank.musteri[i].mTuru == 0){
-			//bireysel
+			if (b!=0) fprintf(pf1, "\n");
+			fprintf(pf1, "Musteri: %d", i+1);
+			fprintf(pf1, "\n\tTc-No: %.lf", aBank.musteri[i].tcNo);
+			fprintf(pf1, "\n\tHesap Sayisi: %d", aBank.musteri[i].hesapSayisi);
+			for (j=0; j<aBank.musteri[i].hesapSayisi; j++){
+				fprintf(pf1, "\n\t\tHesap %d: %d", j+1, aBank.musteri[i].hesap[j].hesapNo);
+				fprintf(pf1, "\n\t\tBakiye: %.2lf", aBank.musteri[i].hesap[j].bakiye);
+			}
+			b=1;
 		}else if (aBank.musteri[i].mTuru == 1){
-			//ticari
+			if (t!=0) fprintf(pf2, "\n");
+			fprintf(pf2, "Musteri: %d", i+1);
+			fprintf(pf2, "\n\tTc-No: %.lf", aBank.musteri[i].tcNo);
+			fprintf(pf2, "\n\tHesap Sayisi: %d", aBank.musteri[i].hesapSayisi);
+			for (j=0; j<aBank.musteri[i].hesapSayisi; j++){
+				fprintf(pf2, "\n\t\tHesap %d: %d", j+1, aBank.musteri[i].hesap[j].hesapNo);
+				fprintf(pf2, "\n\t\tBakiye: %.2lf", aBank.musteri[i].hesap[j].bakiye);
+			}
+			t=1;
 		}
 	}
 	
